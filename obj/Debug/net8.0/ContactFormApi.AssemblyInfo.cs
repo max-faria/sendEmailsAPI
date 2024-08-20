da9aa4796de7c565d164219397603effca4bd482
Max Faria <127763619+max-faria@users.noreply.github.com>
2024-08-20 19:54:04 +1000

    first commit

--- obj/Debug/net8.0/ContactFormApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/ContactFormApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e6bdfc2748a9e5a54af4285276bb99dc271693d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f224236dc2e0319d764a73c38dfd7a6ce88eaad")]
 [assembly: System.Reflection.AssemblyProductAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ContactFormApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
